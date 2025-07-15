c1fb612286104704bb6980ceccd85363159af746
mbeka02 <antonymbeka@gmail.com>
2025-07-15 23:32:20 +0300

    added a graceful shutdown function that runs in another go-routine

--- cmd/server.go
+++ cmd/server.go
@@ -1,12 +1,41 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gorilla/websocket"
 	"github.com/mbeka02/broadcast_server/pkg/server"
 	"github.com/spf13/cobra"
 )
+
+func gracefulShutdown(srv *http.Server, done chan bool) {
+	// Create the context that listens for the interrupt signal from the OS
+	signalCtx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	// Listen for the interrupt signal.
+	<-signalCtx.Done()
+
+	log.Println("shutting down gracefully, press Ctrl+C again to force")
+
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Printf("Server forced to shutdown with error: %v", err)
+	}
+
+	log.Println("Server exiting")
+
+	// Notify the main goroutine that the shutdown is complete
+	done <- true
+}
 
 var serverStartCmd = &cobra.Command{
 	Use:   "start",
@@ -21,7 +50,16 @@ var serverStartCmd = &cobra.Command{
 			},
 		}
 		srv := server.NewServer(port, &upgrader)
-		srv.Run()
+		done := make(chan bool, 1)
+		go gracefulShutdown(srv, done)
+
+		log.Println("the server is listening on port" + srv.Addr)
+		err := srv.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Println("http server error:", err)
+		}
+		<-done
+		log.Println("Graceful shutdown is complete")
 	},
 }
 
